54ccfa34d367b20578a5c5eff049386b89575b4f
DevLucia-21 <21_pg@naver.com>
2026-06-10 18:02:02 +0900

    study(java): 2026-06-10 [텍스트 RPG 전투] 기본 기능 구현

---
+++ assignments/src/main/java/textrpg/Start.java
@@ -0,0 +1,42 @@
+package textrpg;
+
+public class Start {
+    public static void main(String[] args) {
+        Character hero = new Character("용사", 100, 25);
+        Character[] monsters = {
+                new Character("슬라임"),
+                new Character("고블린", 50, 8),
+                new Character("드래곤", 120, 20)
+        };
+
+        boolean clear = true;
+
+        for (Character m : monsters) {
+            System.out.println("\n=== 다음 상대: ===");
+            m.showStatus();
+
+            while (hero.isAlive() && m.isAlive()) {
+                hero.attack(m);
+                m.showStatus();
+
+                if (!m.isAlive()) {
+                    System.out.println("몬스터 쓰러짐!!");
+                    break;
+                }
+
+                m.attack(hero);
+                hero.showStatus();
+            }
+
+            if (!hero.isAlive()) {
+                System.out.println("게임 오버...");
+                clear = false;
+                break;
+            }
+        }
+
+        if (clear) {
+            System.out.println("클리어!!!");
+        }
+    }
+}
