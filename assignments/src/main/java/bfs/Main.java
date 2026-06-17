a2b372c91b75e81398eee7c927019253090fe0bf
dPdms21 <studye20@gmail.com>
2026-06-17 23:55:30 +0900

    study(java): 2026-06-17 [BFS] 최단 거리와 경로 복원 구현

--- assignments/src/main/java/bfs/Main.java
+++ assignments/src/main/java/bfs/Main.java
@@ -4,8 +4,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        boolean[] visited = new boolean[9 + 1];
-        Graph graph = new Graph(9);
+        int vertexCount = 9;
+
+        boolean[] visited = new boolean[vertexCount + 1];
+        int[] dist = new int[vertexCount + 1];
+        int[] parent = new int[vertexCount + 1];
+        Graph graph = new Graph(vertexCount);
 
         graph.addEdge(1, 2);
         graph.addEdge(1, 3);
@@ -27,10 +31,11 @@ public class Main {
         graph.printGraph();
         System.out.println();
 
-        int startVertex = 1;
+        int startVertex = 1; // 3,5
         Queue<Integer> queue = new LinkedList<>();
 
         visited[startVertex] = true;
+        dist[startVertex] = 0;
         queue.add(startVertex);
         System.out.println("정점 " + startVertex + "에서 시작하는 BFS");
 
@@ -41,9 +46,35 @@ public class Main {
             for (int adj : graph.getAdjacencyList()[vertex]) {
                 if (!visited[adj]) {
                     visited[adj] = true;
+                    dist[adj] = dist[vertex] + 1;
+                    parent[adj] = vertex;
                     queue.add(adj);
                 }
             }
+        }
+
+        System.out.println();
+        System.out.println();
+
+        int targetVertex = 9;
+        List<Integer> path = new ArrayList<>();
+
+        for (int vertex=targetVertex; vertex!=0; vertex=parent[vertex]) {
+            path.add(vertex);
+
+            if (vertex == startVertex) {
+                break;
+            }
+        }
+
+        Collections.reverse(path);
+        System.out.println("최단 경로: " + path);
+        System.out.println();
+
+        for (int i = 1; i < dist.length; i++) {
+            System.out.println(
+                    startVertex + " → " + i + " 최단 거리: " + dist[i]
+            );
         }
     }
 }
