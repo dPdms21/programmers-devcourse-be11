92202c4f002d316950fa120a84bc67f183fbc60a
dPdms21 <studye20@gmail.com>
2026-06-17 17:30:23 +0900

    study(java): 2026-06-17 [세마포어 게임] 동시 입장 제한 기본 기능 구현

---
+++ assignments/src/main/java/semaphoregame/Start.java
@@ -0,0 +1,13 @@
+package semaphoregame;
+
+public class Start {
+    public static void main(String[] args) {
+        Dungeon dungeon = new Dungeon(2);
+
+        String[] names = { "전사", "마법사", "궁수", "도적", "성기사" };
+
+        for (String name : names) {
+            new Adventurer(dungeon, name).start();
+        }
+    }
+}
