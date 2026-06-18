1c57bc260eefe8e1224da3914f8ab951261eafbb
dPdms21 <studye20@gmail.com>
2026-06-18 12:25:12 +0900

    study(java): 2026-06-18 [wait/notify] 스레드 간 질문과 응답 동기화 구현

---
+++ assignments/src/main/java/waitnotify/AnswerThread.java
@@ -0,0 +1,22 @@
+package waitnotify;
+
+public class AnswerThread extends Thread {
+    private Chat chat;
+    private String[] answers = { "Hello", "I'm fine, thank you!", "I'm coding in Java" };
+
+    public AnswerThread(Chat chat) {
+        this.chat = chat;
+    }
+
+    @Override
+    public void run() {
+        for (String a : answers) {
+            chat.answer(a);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
