534fbdb6950a6737b610c9e740cbb6c45a8d08a0
dPdms21 <studye20@gmail.com>
2026-06-26 02:07:03 +0900

    solve(java): 2026-06-25 완주하지 못한 선수

---
+++ codingtest/java/week02/완주하지_못한_선수/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+
+        for (String c : completion) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+            }
+        }
+
+        String answer = "";
+
+        for (String p : participant) {
+            if (map.get(p) == 1) {
+                answer = p;
+            }
+        }
+
+        return answer;
+    }
+}
