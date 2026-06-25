6b38a4627967b9b0d71b1a960a93f8971140ddd8
dPdms21 <studye20@gmail.com>
2026-06-26 01:48:37 +0900

    study(java): 2026-06-25 폰켓몬 (getOrDefault)

--- codingtest/java/week02/폰켓몬/Solution.java
+++ codingtest/java/week02/폰켓몬/Solution.java
@@ -4,27 +4,12 @@ class Solution {
     public int solution(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i=0; i<nums.length; i++) {
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], 1);
-            }
-            else {
-                int n = map.get(nums[i]);
-                n++;
-                map.put(nums[i], n);
-            }
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
 
-        int answer = 0;
         int N = nums.length;
 
-        if (map.size() > N/2) {
-            answer = N/2;
-        }
-        else {
-            answer = map.size();
-        }
-
-        return answer;
+        return Math.min(N/2, map.size());
     }
 }
