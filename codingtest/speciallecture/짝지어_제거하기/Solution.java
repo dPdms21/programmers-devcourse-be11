430b4401beb3f4b998d1782492be87033dca9b72
dPdms21 <studye20@gmail.com>
2026-06-25 14:18:10 +0900

    solve(java): 2026-06-25 짝지어 제거하기

---
+++ codingtest/speciallecture/짝지어_제거하기/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+
+            if (stack.peek() == c) {
+                stack.pop();
+            }
+            else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
