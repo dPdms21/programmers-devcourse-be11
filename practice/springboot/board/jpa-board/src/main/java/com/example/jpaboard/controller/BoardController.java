773360c7b08b362797f69d8ba61225da85268a34
dPdms21 <studye20@gmail.com>
2026-07-08 12:52:27 +0900

    study(springboot): 2026-07-08 게시글 CRUD 실습

--- practice/springboot/board/jpa-board/src/main/java/com/example/jpaboard/controller/BoardController.java
+++ practice/springboot/board/jpa-board/src/main/java/com/example/jpaboard/controller/BoardController.java
@@ -4,19 +4,47 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
     @GetMapping("/")
     public String boardList(HttpSession session, Model model) {
         setSession(session, model);
+
         return "board-list";
     }
 
     @GetMapping("/write")
     public String write(HttpSession session, Model model) {
         setSession(session, model);
+
         return "/board-write";
+    }
+
+    @GetMapping("/detail")
+    public String detail(
+            @RequestParam("id") Long id,
+            HttpSession session,
+            Model model
+    ) {
+        setSession(session, model);
+        model.addAttribute("id", id);
+
+        return "/board-detail";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(
+            @PathVariable long id,
+            HttpSession session,
+            Model model
+    ) {
+        setSession(session, model);
+        model.addAttribute("id", id);
+
+        return "/board-update";
     }
 
     private void setSession(HttpSession session, Model model) {
--- practice/springboot/board/jpa-board/src/main/java/com/example/jpaboard/service/BoardService.java
+++ practice/springboot/board/jpa-board/src/main/java/com/example/jpaboard/service/BoardService.java
@@ -2,13 +2,18 @@ package com.example.jpaboard.service;
 
 import com.example.jpaboard.domain.entity.Board;
 import com.example.jpaboard.domain.repository.BoardRepository;
+import com.example.jpaboard.dto.BoardDeleteRequestDto;
+import com.example.jpaboard.dto.BoardUpdateRequestDto;
+import com.example.jpaboard.exception.BoardNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -16,6 +21,7 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final FileService fileService;
 
     public List<Board> getBoardList(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size, Sort.by("id").descending());
@@ -33,5 +39,50 @@ public class BoardService {
 
     public int getTotalBoards() {
         return (int) boardRepository.count();
+    }
+
+    @Transactional
+    public void saveBoard(String userId, String title, String content, MultipartFile file) {
+        String filePath = fileService.storeFile(file);
+
+        boardRepository.save(
+                Board.builder()
+                        .userId(userId)
+                        .title(title)
+                        .content(content)
+                        .filePath(filePath)
+                        .created(LocalDateTime.now())
+                        .build()
+        );
+    }
+
+    public Board getBoardDetail(long id) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> new BoardNotFoundException("[BOARD] 게시글 찾을 수 없음 id: " + id));
+    }
+
+    @Transactional
+    public void updateBoard(long id, BoardUpdateRequestDto dto) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new BoardNotFoundException("[BOARD] 수정할 게시글 찾을 수 없음 id: " + id));
+
+        String filePath = board.getFilePath();
+
+        if (dto.isFileFlag()) { // 파일 변경이 있었을 경우
+            fileService.deleteFile(filePath); // 기존 파일 삭제
+            filePath = fileService.storeFile(dto.getFile()); // 새 파일 저장
+        }
+
+        board.update(dto.getTitle(), dto.getContent(), filePath);
+    }
+
+    @Transactional
+    public void deleteBoard(long id, BoardDeleteRequestDto dto) {
+        if ( !boardRepository.existsById(id) ) {
+            throw new BoardNotFoundException("[BOARD] 삭제할 게시글 찾을 수 없음 id: " + id);
+        }
+
+        boardRepository.deleteById(id);
+        fileService.deleteFile(dto.getFilePath());
     }
 }
