6de0875d16685500a0295b62c35a6e489b3a5eb7
dPdms21 <studye20@gmail.com>
2026-07-18 00:30:00 +0900

    solve(java): 2026-07-17 제일 작은 수 제거하기 (List)

--- codingtest/java/week02/제일_작은_수_제거하기/Solution.java
+++ codingtest/java/week02/제일_작은_수_제거하기/Solution.java
@@ -1,26 +1,33 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr) {
         if (arr.length == 1) {
             return new int[]{-1};
         }
 
+        List<Integer> list = new ArrayList<>();
+
+        for (int i=0; i<arr.length; i++) {
+            list.add(arr[i]);
+        }
+
         int min = arr[0];
         int idx = 0;
 
         for (int i=1; i<arr.length; i++) {
-            if (arr[i] < min) {
+            if (min > arr[i]) {
                 min = arr[i];
                 idx = i;
             }
         }
 
-        int[] answer = new int[arr.length - 1];
-        int j = 0;
+        list.remove(idx);
 
-        for (int i=0; i<arr.length; i++) {
-            if (i != idx) {
-                answer[j++] = arr[i];
-            }
+        int[] answer = new int[list.size()];
+
+        for (int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
         }
 
         return answer;
