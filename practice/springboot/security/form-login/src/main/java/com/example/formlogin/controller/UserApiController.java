b00db6bc2ae350bb3d12b8fac84d496e4f3f09c9
dPdms21 <studye20@gmail.com>
2026-07-27 12:15:40 +0900

    study(springboot): 2026-07-27 회원가입과 보안 설정 실습

---
+++ practice/springboot/security/form-login/src/main/java/com/example/formlogin/controller/UserApiController.java
@@ -0,0 +1,23 @@
+package com.example.formlogin.controller;
+
+import com.example.formlogin.dto.SignUpRequestDto;
+import com.example.formlogin.dto.SignUpResponseDto;
+import com.example.formlogin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserApiController {
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public SignUpResponseDto join(@RequestBody SignUpRequestDto signUpRequestDto) {
+        userService.signUp(signUpRequestDto);
+        return new SignUpResponseDto("/users/login");
+    }
+}
