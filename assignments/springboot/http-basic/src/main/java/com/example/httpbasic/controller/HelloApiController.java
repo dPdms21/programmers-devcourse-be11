88ed9279aaf6cac26945d1f0d1a000726db204ea
dPdms21 <studye20@gmail.com>
2026-07-28 14:49:01 +0900

    study(springboot): 2026-07-28 [HTTP Basic] 기본 인증 흐름 구현

---
+++ assignments/springboot/http-basic/src/main/java/com/example/httpbasic/controller/HelloApiController.java
@@ -0,0 +1,12 @@
+package com.example.httpbasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloApiController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
