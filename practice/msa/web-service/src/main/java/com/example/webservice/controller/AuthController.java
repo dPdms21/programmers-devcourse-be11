59a799cdafdf18d70170a8e606ead54614ba50f5
dPdms21 <studye20@gmail.com>
2026-09-22 14:46:05 +0900

    study(springboot): 2026-09-22 MSA Docker 환경 구성 및 서비스 연동 실습

--- practice/msa/web-service/src/main/java/com/example/webservice/controller/AuthController.java
+++ practice/msa/web-service/src/main/java/com/example/webservice/controller/AuthController.java
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AuthController {
     @GetMapping("/join")
     public String join() {
-        return "/auth/sign-up";
+        return "auth/sign-up";
     }
 
     @GetMapping("/login")
     public String login() {
-        return "/auth/login";
+        return "auth/login";
     }
 
     @GetMapping("/oauth-join")
     public String oauthJoin() {
-        return "/auth/oauth-join";
+        return "auth/oauth-join";
     }
 }
